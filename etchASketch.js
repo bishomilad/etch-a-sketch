03a189ada75b0232d8d83d4e988a348c02cafe2d
Bishoy Milad <123456789+bishomilad72@yahoo.com>
2026-02-24 17:01:37 +0200

    Adding the working logic of sketching

--- etchASketch.js
+++ etchASketch.js
@@ -3,19 +3,28 @@ let gridSize = 16;
 const gridContainer = document.querySelector(".container");
 
 const fillContainer = function(){
-    //create a number of gridsize rows of gridsize of divs
     const gridRowDiv = document.createElement("div");
+    gridRowDiv.classList.add("gridRowDiv");
     const gridrow =[];
+    //create a row of gridsize of divs
     for(let i=0;i<gridSize;i++){
         const gridDiv = document.createElement("div");
         gridDiv.classList.add("gridDiv");
         gridrow.push(gridDiv);
         gridRowDiv.appendChild(gridDiv);
     }
-    //cloning the row each iteration
+    //cloning that row each iteration
     for(let i=0;i<gridSize;i++){
-        
+        gridContainer.appendChild(gridRowDiv.cloneNode(true));
 
     }
 
 }
+
+gridContainer.addEventListener("mouseover", e =>{
+    if(e.target.classList.contains("gridDiv")){
+        e.target.classList.add("hoveredOn");
+    }
+})
+
+fillContainer();
